21c76f461de5d90b3a22bc95928144317fe34d19
Jake <hukarijake@gmail.com>
2026-05-23 17:26:25 -0600

    Update Sp3arParvus.lua

--- Sp3arParvus.lua
+++ Sp3arParvus.lua
@@ -1954,7 +1954,7 @@ function UI.Notify(title, text, duration)
     icon.BackgroundTransparency = 1
     icon.ImageTransparency = 1
     
-    local iconUrl = "https://www.pingbird.xyz/f/Sp3arParvus"
+    local iconUrl = "https://www.pingbird.xyz/f/Sp3arParvus.png"
     
     local function encodeParam(str)
         if str == nil or str == "" then return "unknown" end
